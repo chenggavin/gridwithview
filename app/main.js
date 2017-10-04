21817ac0defb098673875e0f9f303ec8e5dc380f
Gavin Cheng <gavincheng01@gmail.com>
2017-10-04 14:02:51 -0400

    Working switchable grid

--- app/main.js
+++ app/main.js
@@ -1,35 +1,31 @@
 var demo = new Vue({
 
-	el: '#main',
+  el: '#main',
 
-	data: {
-		layout: 'grid',
-		cats: ['a','b']
-	},
+  data: {
+    layout: 'grid',
+    cats: []
+  },
 
-	mounted: function() {
+  mounted: function() {
 
-		var self = this;
+    var self = this;
 
-		axios.get("http://thecatapi.com/api/images/get?format=xml&results_per_page=6")
-			.then(function (response) {
+    axios.get("http://thecatapi.com/api/images/get?format=xml&results_per_page=6")
+      .then(function (response) {
 
+        var parser = new DOMParser();
+        var data = parser.parseFromString(response.data, "text/xml");
 
-			var parser = new DOMParser();
-			var data = parser.parseFromString(response.data, "text/xml");
+        Array.from(data.getElementsByTagName('url'))
+        .forEach(function(url) {
+          self.cats.push(url.innerHTML);
+        });
+        
+      });
 
-			Array.from(data.getElementsByTagName('url'))
-			.forEach(function(url) {
+    console.log(self.cats);
 
-
-
-
-			});
-
-
-			});
-
-	}
-
+  }
 
 });
