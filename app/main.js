5bd6dbf2ea4643097b8f2d946c207f5187608714
Gavin Cheng <gavincheng01@gmail.com>
2017-10-04 12:07:07 -0400

    allowed for api's

--- app/main.js
+++ app/main.js
@@ -1,19 +1,35 @@
 var demo = new Vue({
 
-el: '#main',
+	el: '#main',
 
-data: {
+	data: {
+		layout: 'grid',
+		cats: ['a','b']
+	},
 
-	layout: 'list'
+	mounted: function() {
+
+		var self = this;
+
+		axios.get("http://thecatapi.com/api/images/get?format=xml&results_per_page=6")
+			.then(function (response) {
 
 
-},
+			var parser = new DOMParser();
+			var data = parser.parseFromString(response.data, "text/xml");
+
+			Array.from(data.getElementsByTagName('url'))
+			.forEach(function(url) {
 
 
-methods: {
 
 
-}
+			});
+
+
+			});
+
+	}
 
 
 });
